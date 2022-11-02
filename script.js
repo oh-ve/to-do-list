1fc21d252d5e43293fe0ded60d314d33f7c03c47
apollosport <apollosport84@gmail.com>
2022-11-02 16:53:15 +0100

    checkbox update

--- script.js
+++ script.js
@@ -3,16 +3,21 @@ let button = document.getElementById("button");
 const ulElement = document.querySelector("ul");
 const areaField = document.getElementById("firstinput");
 console.log(areaField);
-  let enterClick = areaField.onkeypress = function(e) {     
-  if (e.key === 'Enter'){
-    document.getElementById("button").click(); }
-};
+let enterClick = (areaField.onkeypress = function (e) {
+  if (e.key === "Enter") {
+    document.getElementById("button").click();
+  }
+});
 let inputHandler = button.addEventListener("click", () => {
   let input = document.querySelector(".input").value;
   document.querySelector(".input").value = "";
   let liListener = document.querySelector("li");
   const divElement = document.createElement("div");
   ulElement.appendChild(divElement);
+  const checkElement = document.createElement("input");
+  checkElement.type = "checkbox";
+  checkElement.className = "checked0";
+  const divTextElement = document.createElement("div");
   const liElement = document.createElement("li");
   liElement.className = "notdone";
   liElement.className = "notdone0";
@@ -20,42 +25,39 @@ let inputHandler = button.addEventListener("click", () => {
   const buttonElement = document.createElement("input");
   buttonElement.type = "button";
   buttonElement.value = "delete";
-  buttonElement.className = "close";   
-  const buttonElement2 = document.createElement("input");
-  buttonElement2.type = "button";
-  buttonElement2.value = "edit";
-  buttonElement2.className = "edit";
-  divElement.appendChild(liElement);
+  buttonElement.className = "close";
+  divElement.appendChild(checkElement);
+  divElement.appendChild(divTextElement);
+  divTextElement.appendChild(liElement);
   divElement.appendChild(buttonElement);
-  divElement.appendChild(buttonElement2);
   let close = document.getElementsByClassName("close");
   let inputHandler2 = buttonElement.addEventListener("click", () => {
     divElement.remove();
   });
-  let edit = document.getElementsByClassName("edit");
-  let inputHandler3 = buttonElement2.addEventListener("click", () => {
+  let done = document.getElementsByClassName("checkbox");
+  checkElement.addEventListener("change", () => {
+    if (checkElement.checked) {
+      liElement.style.textDecoration = "line-through";
+    } else {
+      liElement.style.textDecoration = "none";
+    }
+  });
+
+  let inputHandler5 = liElement.addEventListener("click", () => {
+    liElement.style.display = "none";
     console.log("edit button clicked");
     let editArea = document.createElement("input");
     editArea.className = "editField";
     editArea.type = "text";
     editArea.value = liElement.innerText;
-    divElement.appendChild(editArea);
-      let inputHandler4 = editArea.addEventListener("keypress", (e) => {        
-        if (e.key === 'Enter'){
+    divTextElement.appendChild(editArea);
+    let inputHandler4 = editArea.addEventListener("keypress", (e) => {
+      if (e.key === "Enter") {
         let input2 = document.querySelector(".editField").value;
         liElement.innerText = input2;
         editArea.remove();
+        liElement.style.display = "";
       }
-    })  
+    });
   });
-  let done = document.getElementsByClassName("notdone");
-  let inputHandler5 = liElement.addEventListener("click", () => {
-    if (liElement.className === "notdone0"){
-      liElement.style.textDecoration = "line-through";
-      liElement.classList.remove("notdone0");
-    } else {
-      liElement.style.textDecoration = "none";
-      liElement.className = "notdone0";
-    }
-  })
-})
+});
