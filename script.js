37712ac52cbae99a4a003e831173e8a0a34e2377
apollosport <apollosport84@gmail.com>
2022-11-02 21:46:01 +0100

    local storage

--- script.js
+++ script.js
@@ -1,63 +1,92 @@
-//let arrayOfTodos = [];
-let button = document.getElementById("button");
+/*class Object {
+  construtor(msg = "hallo", checked = false) {
+    this.msg = msg;
+    this.checked = checked;
+  }
+  objectGetter() {
+    return this.msg;
+  }
+}
+
+const storage = [];
+
+/*function object(msg, checked = false) {
+  this.msg = msg;
+  this.checked = checked;
+}*/
+localStorage.clear();
+
+const button = document.getElementById("button");
 const ulElement = document.querySelector("ul");
 const areaField = document.getElementById("firstinput");
-console.log(areaField);
-let enterClick = (areaField.onkeypress = function (e) {
+let taskCounter = 0;
+areaField.addEventListener("keypress", (e) => {
   if (e.key === "Enter") {
     document.getElementById("button").click();
   }
 });
-let inputHandler = button.addEventListener("click", () => {
+button.addEventListener("click", () => {
   let input = document.querySelector(".input").value;
+  if (!input) {
+    alert("Please be serious. You definitely have something to do!");
+    return;
+  }
+  taskCounter++;
+  let tempCounter = `tasknr.${taskCounter}`;
+  console.log(localStorage);
+  window.localStorage.setItem(tempCounter, `${input}0`); //add 0 to input because not done with the task
   document.querySelector(".input").value = "";
-  let liListener = document.querySelector("li");
   const divElement = document.createElement("div");
-  ulElement.appendChild(divElement);
+  divElement.className = `tasknr.${taskCounter}`;
   const checkElement = document.createElement("input");
   checkElement.type = "checkbox";
-  checkElement.className = "checked0";
   const divTextElement = document.createElement("div");
   const liElement = document.createElement("li");
-  liElement.className = "notdone";
-  liElement.className = "notdone0";
   liElement.innerText = input;
   const buttonElement = document.createElement("input");
   buttonElement.type = "button";
   buttonElement.value = "delete";
-  buttonElement.className = "close";
   divElement.appendChild(checkElement);
   divElement.appendChild(divTextElement);
   divTextElement.appendChild(liElement);
   divElement.appendChild(buttonElement);
-  let close = document.getElementsByClassName("close");
-  let inputHandler2 = buttonElement.addEventListener("click", () => {
+  buttonElement.addEventListener("click", () => {
+    localStorage.removeItem(tempCounter);
+    console.log(tempCounter);
     divElement.remove();
+    console.log(localStorage);
   });
-  let done = document.getElementsByClassName("checkbox");
+  document.getElementsByClassName("checkbox");
   checkElement.addEventListener("change", () => {
     if (checkElement.checked) {
       liElement.style.textDecoration = "line-through";
+      localStorage.setItem(tempCounter, `${input}1`);
+      console.log(localStorage);
     } else {
       liElement.style.textDecoration = "none";
+      localStorage.setItem(tempCounter, `${input}0`);
+      console.log(localStorage);
     }
   });
 
-  let inputHandler5 = liElement.addEventListener("click", () => {
+  liElement.addEventListener("click", () => {
     liElement.style.display = "none";
-    console.log("edit button clicked");
     let editArea = document.createElement("input");
     editArea.className = "editField";
     editArea.type = "text";
     editArea.value = liElement.innerText;
     divTextElement.appendChild(editArea);
-    let inputHandler4 = editArea.addEventListener("keypress", (e) => {
+    editArea.addEventListener("keypress", (e) => {
       if (e.key === "Enter") {
         let input2 = document.querySelector(".editField").value;
         liElement.innerText = input2;
         editArea.remove();
         liElement.style.display = "";
+        //if ()
       }
     });
   });
+  ulElement.appendChild(divElement);
 });
+
+console.log("HEREEE", ulElement);
